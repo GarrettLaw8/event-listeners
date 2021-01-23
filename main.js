9382cc01ec8643937511e8f5013de7d4f6ff6786
Garrett Lawson <garrettlaw8@gmail.com>
2021-01-23 09:56:13 -0600

    Object lesson

--- main.js
+++ main.js
@@ -1,1 +1,89 @@
-console.log ("hello world!")
+//console.log("hello world!")
+
+
+const firstObject = {
+  
+};
+
+
+// const user = {
+//   username: "biz.markie",
+//   password: "qweasdzxc",
+//   lovesJs: true, 
+//   favoriteNumber: 42, 
+// }
+
+
+//accesing values in an object: 
+
+//dotnotation 
+// console.log(user.lovesJs);
+// console.log(user.username);
+// console.log(user.favoriteNumber)
+//bracket notation 
+// const password = "password"
+// console.log(user[password]); 
+
+// console.log(user['username']);
+
+// console.log(user['favoriteNumber']);
+
+
+
+
+// Assinging Values
+
+// newUser = {
+//   isNew: true,
+// };
+
+// newUser.username = "garrettLaw8";
+
+// console.log(newUser.username);
+
+// newUser['password'] = "qweasdzxc";
+
+// console.log(newUser)
+
+// functions in an object
+
+
+// const newObject = {
+//   userName: "GarrettLaw8",
+//   sayHello: function () {
+//     console.log("hello")
+//   }
+// }
+
+// console.log(newObject.sayHello())
+
+
+
+// const newObject = {
+//   userName: "GarrettLaw8",
+//   sayHello: function () {
+//     console.log(`${this.userName} says Hello`)
+//   }
+// }
+
+// newObject.userName = "Garrett" 
+//***reassigns the value of username from garrettLaw8 to garrett*/
+
+// console.log(newObject.sayHello())
+
+
+
+//iterate an object
+
+const newUser2 = {
+  userName: "bigG8888",
+  password: "qwertasdfgbrg",
+  lovesJs: true,
+  favoriteNumber: 12,
+};
+
+
+for (let key in newUser2) {
+  //console.log(key)// all the keys
+  console.log(newUser2[key]);
+}
